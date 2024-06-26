e3c2e978fefd95e92344c76ed5199cfe05bae32e
AmjadAlian <amjadalian14@gmail.com>
2024-06-26 07:45:23 +0300

    Add README.md.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using API_Crud.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> studnets = new List<Product>{
+        new Product {Id=1, Name="example1",description="example1" },
+        new Product {Id=2, Name="example2",description=" example2" },
+        new Product {Id=3, Name="example3",description="example3" },
+        };
+
+
+        [HttpGet]
+        public IActionResult GetAll ()
+        {
+            return Ok(studnets);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById (int id) {
+         
+            var Product = studnets.FirstOrDefault(Product=>Product.Id == id);
+        
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Product);
+        }
+
+        [HttpPost]
+
+        public IActionResult Create (Product request) {
+        
+            if (request == null)
+            {
+                return BadRequest();
+            }
+            var Product = new Product { Id = request.Id , Name = request.Name , description=request.description };
+
+            studnets.Add(Product);
+            return Ok(Product);
+
+        }
+        [HttpPut("{id}")]
+
+        public IActionResult Update (int id,Product request)
+        {
+            var currentProduct = studnets.FirstOrDefault(Product=>Product.Id == id);
+
+            if(currentProduct == null)
+            {
+                return NotFound();
+            }
+            
+            currentProduct.Name = request.Name;
+            currentProduct.description = request.description;
+
+            return Ok(currentProduct);
+        }
+        [HttpDelete ("{id}")]
+
+        public IActionResult Delete (int id) {
+        var Product = studnets.FirstOrDefault(Product=>Product.Id == id);
+            if (Product is null)
+            {
+                return NotFound();
+            }
+            studnets.Remove(Product);
+            return Ok();
+        }
+
+    } 
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Crud.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string description { get; set; }
+    }
+}
